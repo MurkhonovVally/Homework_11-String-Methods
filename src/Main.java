af4f91c59acbcffdfc2bfeb615a44475de3b4fc2
Vally <murkhonovbatoha@gmail.com>
2023-01-28 09:48:47 +0800

    Body

---
+++ src/Main.java
@@ -0,0 +1,21 @@
+public class Main {
+    public static void main(String[] args) {
+        task1();
+        task2();
+        task3();
+    }
+
+    public static void task1() {
+        System.out.println((char) 27 + "[34m----------Задача 1----------" + (char) 27 + "[0m");
+    }
+    public static void task2() {
+        System.out.println((char) 27 + "[32m--------Конец задачи--------" + (char) 27 + "[0m");
+        System.out.println(" ");
+        System.out.println((char) 27 + "[34m----------Задача 2----------" + (char) 27 + "[0m");
+    }
+    public static void task3() {
+        System.out.println((char) 27 + "[32m--------Конец задачи--------" + (char) 27 + "[0m");
+        System.out.println(" ");
+        System.out.println((char) 27 + "[34m----------Задача 3----------" + (char) 27 + "[0m");
+    }
+}
